2bc147e538a12dcb6d73b3ae66bad33f89755b21
강은빈 <kub0803@naver.com>
2021-05-11 23:32:11 +0900

    [ADD] 라우터 추가

--- alavez/src/router/Router.tsx
+++ alavez/src/router/Router.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { Login, Main } from "../components/index";
+import { Login, Main, MyPage, SignUp } from "../components/index";
 
 const Router = () => {
   return (
@@ -9,6 +9,8 @@ const Router = () => {
         <Switch>
           <Route path="/" component={Main} exact />
           <Route path="/login" component={Login} exact />
+          <Route path="/sign-up" component={SignUp} exact />
+          <Route path="/my-page" component={MyPage} exact />
         </Switch>
       </BrowserRouter>
     </>
