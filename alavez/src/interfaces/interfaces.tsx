f8ed53a460f3e5e05eeca98216b999ee4eacf13f
강은빈 <kub0803@naver.com>
2021-06-14 23:50:35 +0900

    [ADD] 인터페이스 추가

--- alavez/src/interfaces/interfaces.tsx
+++ alavez/src/interfaces/interfaces.tsx
@@ -1,22 +1,27 @@
 import { Method } from "axios";
 
-export interface requestParams{
-    method: Method,
-    url: String,
-    header: Object,
-    data: any
+export interface requestParams {
+  method: Method;
+  url: String;
+  header: Object;
+  data: any;
 }
 
-export interface inputsState{
-    name: string,
-    nick: string,
-    email: string,
-    confirm: string,
-    password: string,
-    re_password: string,
-    file: string,
+export interface inputsState {
+  name: string;
+  nick: string;
+  email: string;
+  confirm: string;
+  password: string;
+  re_password: string;
+  file: string;
 }
 
-export interface a{
-    data : inputsState
+export interface a {
+  data: inputsState;
+}
+
+export interface button {
+  id: number;
+  button: string;
 }
