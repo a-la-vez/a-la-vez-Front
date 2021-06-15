1023f5ba0e810c4a8be99e63ae48249a078d7956
강은빈 <kub0803@naver.com>
2021-06-15 23:49:33 +0900

    [ADD] 모달 index 추가

--- alavez/src/components/index.ts
+++ alavez/src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Header } from "./header/Header";
 export { default as Login } from "./signIn/Login";
 export { default as SignUp } from "./signIn/SignUp";
+export { default as ConfirmModal } from "./signIn/ConfirmModal";
 export { default as Main } from "./mainPage/Main";
 export { default as MyPage } from "./myPage/myPage";
