59ca5e90305edd280da2779650028d7fc69ce73d
강은빈 <kub0803@naver.com>
2021-06-16 20:20:54 +0900

    [ADD] 닫기 이미지 추가

--- alavez/src/assets/index.ts
+++ alavez/src/assets/index.ts
@@ -1,3 +1,4 @@
 export { default as iphone } from "./iphone.webp";
 export { default as heart } from "./heart.svg";
 export { default as heartCheck } from "./heartCheck.svg";
+export { default as cancel } from "./cancel.svg";
