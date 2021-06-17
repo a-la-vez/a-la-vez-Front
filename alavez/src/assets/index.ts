972c8aa248af93024b8a3653e7c3372473b2ee3a
강은빈 <kub0803@naver.com>
2021-06-17 23:36:34 +0900

    [ADD] 이미지 추가

--- alavez/src/assets/index.ts
+++ alavez/src/assets/index.ts
@@ -2,3 +2,7 @@ export { default as iphone } from "./iphone.webp";
 export { default as heart } from "./heart.svg";
 export { default as heartCheck } from "./heartCheck.svg";
 export { default as cancel } from "./cancel.svg";
+export { default as location } from "./location.svg";
+export { default as company } from "./company.svg";
+export { default as glass } from "./glass.svg";
+export { default as groupImg } from "./groupImg.jpg";
