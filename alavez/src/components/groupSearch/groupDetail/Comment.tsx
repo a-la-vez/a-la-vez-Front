b386c8f6860e73de7e00a80ce059cef22d230e52
강은빈 <kub0803@naver.com>
2021-06-20 09:10:53 +0900

    [ADD] 댓글 텀포넌트 추가

---
+++ alavez/src/components/groupSearch/groupDetail/Comment.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import * as S from "./style";
+
+const comment = (comments: any, index: number) => {
+  return (
+    <S.Comment key={index}>
+      <div className="user-info">
+        <img src={comments.comments.ImagePath} alt="사용자 프로필 사진" />
+        <span>{comments.comments.UserName}</span>
+        <span>{comments.comments.CreatedAt}</span>
+      </div>
+      <div className="content">{comments.comments.Content}</div>
+    </S.Comment>
+  );
+};
+
+export default comment;
