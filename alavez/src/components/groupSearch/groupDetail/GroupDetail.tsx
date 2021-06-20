53c6dc693817287974dc98d678a5af6f0ae806e3
강은빈 <kub0803@naver.com>
2021-06-20 09:12:00 +0900

    [MODIFY] 그룹 디테일 UI 완성

--- alavez/src/components/groupSearch/groupDetail/GroupDetail.tsx
+++ alavez/src/components/groupSearch/groupDetail/GroupDetail.tsx
@@ -1,24 +1,110 @@
-import React from "react";
-import { groupImg2 } from "../../../assets";
+import { useState } from "react";
+import { groupImg2, heart, heartCheck } from "../../../assets";
+import { GroupDetailType, CommentType } from "../../../interfaces/group";
+import Comment from "./Comment";
 import * as S from "./style";
 
-const groupDetail = [{}];
+const groupDetail: GroupDetailType = {
+  Id: 1,
+  Title: "대덕소프트웨어마이스터고등학교를 소개해요!",
+  describe: "모집해요!!!",
+  Period: "3일간",
+  ImagePath: groupImg2,
+  Content: "같이 공부할 사람 모집합니다.",
+  CreatedAt: "2020-20-20",
+  UpdatedAt: "2020-20-20",
+  Category: "전공",
+};
+
+const commentDummy: CommentType[] = [
+  {
+    ImagePath: groupImg2,
+    Content: "어떤 방법으로 모집되나요",
+    CreatedAt: "2020-20-20",
+    UserName: "은빈",
+  },
+  {
+    ImagePath: groupImg2,
+    Content: "어떤 방법으로 모집되나요",
+    CreatedAt: "2020-20-20",
+    UserName: "은빈",
+  },
+  {
+    ImagePath: groupImg2,
+    Content: "어떤 방법으로 모집되나요",
+    CreatedAt: "2020-20-20",
+    UserName: "은빈",
+  },
+  {
+    ImagePath: groupImg2,
+    Content: "어떤 방법으로 모집되나요",
+    CreatedAt: "2020-20-20",
+    UserName: "은빈",
+  },
+];
 
 const GroupDetail = () => {
+  const [heartClick, setHeartClick] = useState(heart);
+  const [count, setCount] = useState<boolean>(false);
+
+  // 하트 채우기
+  const heartClickhandler = () => {
+    if (count) {
+      setHeartClick(heart);
+    } else {
+      setHeartClick(heartCheck);
+    }
+    setCount(!count);
+  };
   return (
     <S.MainWrapper>
       <S.GroupDetailContent>
+        <S.GroupDetailHeader>
+          <div className="category">
+            <div className="category-item">{groupDetail.Category}</div>
+          </div>
+          <div className="date">
+            <span>작성일 : {groupDetail.CreatedAt}</span>
+            <span>수정일 : {groupDetail.UpdatedAt}</span>
+          </div>
+        </S.GroupDetailHeader>
         <S.TopContent>
           <div className="group-img">
-            <img src={groupImg2} alt="그룹이미지" />
+            <img src={groupDetail.ImagePath} alt="그룹이미지" />
           </div>
           <div className="side-detail">
             <div className="side-detail-top">
-              <p></p>
+              <p>{groupDetail.Title}</p>
+              <p>모집기간 : {groupDetail.Period}</p>
+            </div>
+            <div className="side-detail-mid">
+              <p>{groupDetail.Content}</p>
+              <div className="button-item">
+                <img
+                  src={heartClick}
+                  alt="즐겨찾기 아이콘"
+                  onClick={heartClickhandler}
+                />
+                <button>신청하기</button>
+              </div>
             </div>
           </div>
         </S.TopContent>
-        <S.BottomContent></S.BottomContent>
+        <S.BottomContent>
+          <S.CommentWrapper>
+            <S.CommentHeader>
+              <span>댓글</span>
+              <span>2개</span>
+            </S.CommentHeader>
+            <S.CommentInput>
+              <input type="text" placeholder="댓글을 작성해주세요" />
+            </S.CommentInput>
+            {commentDummy.map((comment: CommentType, index: number): any => (
+              <Comment key={index} comments={comment} />
+            ))}
+            <button className="comment-more">더보기</button>
+          </S.CommentWrapper>
+        </S.BottomContent>
       </S.GroupDetailContent>
     </S.MainWrapper>
   );
--- alavez/src/components/groupSearch/groupDetail/style.ts
+++ alavez/src/components/groupSearch/groupDetail/style.ts
@@ -4,25 +4,50 @@ export const MainWrapper = styled.div`
   width: 55%;
   height: 100%;
   margin: 85px auto;
-  border: 1px solid red;
 `;
 
 export const GroupDetailContent = styled.div`
   width: 100%;
-  border: 1px solid red;
+`;
+
+export const GroupDetailHeader = styled.div`
+  width: 100%;
+  height: 30px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  color: #4a4a4a;
+  font-weight: 300;
+  margin-bottom: 20px;
+
+  .category {
+    .category-item {
+      padding: 6px;
+      background-color: #6f2dff;
+      color: white;
+      border-radius: 10px;
+    }
+  }
+
+  .date {
+    span {
+      margin: 0 10px;
+    }
+  }
 `;
 
 export const TopContent = styled.div`
   width: 100%;
   display: flex;
   flex-direction: row;
+  margin-bottom: 50px;
 
   .group-img {
     width: 55%;
-    height: 500px;
 
     & img {
       width: 100%;
+      height: 400px;
       object-fit: cover;
     }
   }
@@ -30,9 +55,152 @@ export const TopContent = styled.div`
   .side-detail {
     margin-left: 5%;
     width: 40%;
-    height: 500px;
-    border: 1px solid red;
+  }
+
+  .side-detail-top {
+    width: 100%;
+    height: 110px;
+    border-top: 1px solid #80808069;
+    border-bottom: 1px solid #80808069;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    p:nth-child(1) {
+      font-weight: 500;
+      font-size: 20px;
+      margin-top: 10px;
+    }
+
+    p:nth-child(2) {
+      float: right;
+      margin-bottom: 10px;
+    }
+  }
+
+  .side-detail-mid {
+    border-bottom: 1px solid #80808069;
+    height: 285px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    & p {
+      margin-top: 10px;
+    }
+  }
+
+  .button-item {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+
+    //즐겨찾기 아이콘
+    & img {
+      cursor: pointer;
+      width: 30px;
+      margin: 5px;
+    }
+
+    //신청하기 버튼
+    & button {
+      margin-bottom: 5px;
+      padding: 8px;
+      cursor: pointer;
+      width: 100%;
+      outline: none;
+      border: none;
+      background-color: #ff9aac94;
+      color: white;
+      border-radius: 4px;
+
+      :hover {
+        background-color: #6f2dff;
+        transition: all 0.6s ease-out;
+      }
+    }
   }
 `;
 
-export const BottomContent = styled.div``;
+export const BottomContent = styled.div`
+  width: 100%;
+  border-top: 1px solid #80808069;
+`;
+
+export const CommentWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+
+  //더보기 버튼
+  .comment-more {
+    cursor: pointer;
+    margin-top: 20px;
+    width: 100%;
+    height: 40px;
+    padding: 5px;
+    border: 1px solid #e4e5ed;
+    background: #fff;
+    color: #9a9ba7;
+    outline: none;
+    font-size: 15px;
+  }
+`;
+
+export const CommentHeader = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+  border-bottom: 1px solid #80808069;
+
+  & span {
+    margin: 0 5px;
+  }
+`;
+
+export const CommentInput = styled.div`
+  width: 50%;
+  margin-top: 10px;
+
+  & input {
+    padding: 3px 10px;
+    width: 90%;
+    height: 30px;
+    outline: none;
+    border: 1px solid #80808047;
+    border-radius: 11px;
+    background-color: #b5b3b314;
+  }
+`;
+
+export const Comment = styled.div`
+  width: 100%;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border-bottom: 1px solid #80808069;
+
+  .user-info {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    & img {
+      width: 40px;
+      height: 40px;
+      border-radius: 50px;
+    }
+    & span {
+      margin: 0 8px;
+      font-size: 15px;
+      color: #9a9ba7;
+    }
+  }
+
+  .content {
+    margin-top: 20px;
+    color: #555969;
+    line-height: 1.5;
+  }
+`;
