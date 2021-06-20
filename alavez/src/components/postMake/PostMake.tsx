10aca18ada205ae2a72c2fb2d9f89802aaf5cd38
강은빈 <kub0803@naver.com>
2021-06-20 09:11:07 +0900

    [ADD] 게시글 등록 컴포넌트 추가

---
+++ alavez/src/components/postMake/PostMake.tsx
@@ -0,0 +1,11 @@
+import * as S from "./style";
+
+const PostMake = () => {
+  return (
+    <div className="container">
+      <S.PostHeader></S.PostHeader>
+    </div>
+  );
+};
+
+export default PostMake;
