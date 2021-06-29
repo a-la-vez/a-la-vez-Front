fb85ae301c619b548db3122078b868ccc9a38ecd
강은빈 <kub0803@naver.com>
2021-06-29 23:51:53 +0900

    [ADD] 인터페이스 추가

--- alavez/src/interfaces/group.ts
+++ alavez/src/interfaces/group.ts
@@ -28,3 +28,88 @@ export interface CommentType {
 export interface CommentPropsState extends CommentType {
   comment: [];
 }
+
+export const button: ButtonType[] = [
+  {
+    id: 1,
+    name: "전체",
+  },
+  {
+    id: 2,
+    name: "수학",
+  },
+  {
+    id: 3,
+    name: "영어",
+  },
+  {
+    id: 4,
+    name: "프로젝트",
+  },
+  {
+    id: 5,
+    name: "프론트",
+  },
+  {
+    id: 6,
+    name: "백엔드",
+  },
+  {
+    id: 7,
+    name: "안드로이드",
+  },
+  {
+    id: 10,
+    name: "IOS",
+  },
+  {
+    id: 11,
+    name: "AI",
+  },
+  {
+    id: 12,
+    name: "게임",
+  },
+  {
+    id: 13,
+    name: "기타",
+  },
+];
+
+export const listDummy: ListDummyType[] = [
+  {
+    Title: "대덕마이스터고등학교 공부할 사람!!!",
+    describe: "인원 6명",
+    Period: "2020202",
+  },
+  {
+    Title: "대덕마이스터고등학교 공부할 사람!!!",
+    describe: "인원 6명",
+    Period: "2020202",
+  },
+  {
+    Title: "대덕마이스터고등학교 공부할 사람!!!",
+    describe: "인원 6명",
+    Period: "2020202",
+  },
+  {
+    Title: "대덕마이스터고등학교 공부할 사람!!!",
+    describe: "인원 6명",
+    Period: "2020202",
+  },
+  {
+    Title: "대덕마이스터고등학교 공부할 사람!!!",
+    describe: "인원 6명",
+    Period: "2020202",
+  },
+  {
+    Title: "대덕마이스터고등학교 공부할 사람!!!",
+    describe: "인원 6명",
+    Period: "2020202",
+  },
+  {
+    Title: "대덕마이스터고등학교 공부할 사람!!!",
+    describe: "인원 6명",
+    Period: "2020202",
+  },
+];
