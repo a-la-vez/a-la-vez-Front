935bea391bf95c8c501583dcf49d312abd359225
강은빈 <kub0803@naver.com>
2021-06-29 23:51:09 +0900

    [MODIFY] 게시글 생성 UI

--- alavez/src/components/postMake/PostMake.tsx
+++ alavez/src/components/postMake/PostMake.tsx
@@ -1,10 +1,132 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { button } from "../../interfaces/group";
 import * as S from "./style";
 
 const PostMake = () => {
+  const [fileUrl, setFileUrl] = useState<string>("");
+  const [selected, setSelected] = useState<number>(1);
+  const [post, setPost] = useState<boolean>(false);
+
+  const [inputs, setInputs] = useState({
+    title: "",
+    content: "",
+    number: "",
+    daedline: "",
+  });
+
+  const { title, content, daedline } = inputs;
+
+  useEffect(() => {
+    
+  });
+  if (inputs !== null) {
+    setPost(!post);
+  }
+
+  const onChange = (e: any) => {
+    const { value, name } = e.target;
+
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const handleSumit = (e: any) => {
+    e.preventDefault();
+
+    setInputs({
+      title: "",
+      content: "",
+      number: "",
+      daedline: "",
+    });
+
+    console.log(inputs);
+  };
+
+  const categoryClickHandler = (button: any) => {
+    setSelected(button.id);
+  };
+
+  function processImage(e: React.ChangeEventHandler<HTMLInputElement> | any) {
+    const imageFile = e.target.files[0];
+    const imageUrl = URL.createObjectURL(imageFile);
+    setFileUrl(imageUrl);
+  }
+
   return (
-    <div className="container">
-      <S.PostHeader></S.PostHeader>
-    </div>
+    <S.PostWrapper onSubmit={handleSumit}>
+      <S.PostHeader>
+        <S.Category>
+          {button.map((button, index) => (
+            <button
+              className="default-button"
+              key={index}
+              style={{
+                backgroundColor:
+                  button.id === selected
+                    ? "#6f2dff"
+                    : "rgba(128, 128, 128, 0.16)",
+                color: button.id === selected ? "white" : "black",
+              }}
+              onClick={() => categoryClickHandler(button)}
+            >
+              {button.name}
+            </button>
+          ))}
+        </S.Category>
+      </S.PostHeader>
+      <S.ContentWrapper>
+        <S.ContentTitle
+          placeholder="스터디 모집글을 입력해주세요"
+          onChange={onChange}
+          value={title}
+          name="title"
+        ></S.ContentTitle>
+        <S.Content>
+          <S.ContentSub
+            placeholder="스터디 모집 내용을 입력해주세요"
+            onChange={onChange}
+            value={content}
+            name="content"
+          ></S.ContentSub>
+          <S.ContentSide>
+            <div className="content-side">
+              <span>모집인원</span>
+              <select name="number" onChange={onChange} defaultValue="명">
+                <option value="" selected disabled hidden>
+                  선택해주세요.
+                </option>
+                <option value="000">제한 없음</option>
+                <option value="100">1명 이하</option>
+                <option value="200">2 ~ 3명</option>
+                <option value="300">4 ~ 5명</option>
+                <option value="400">6 ~ 7명</option>
+                <option value="500">8명 이상</option>
+              </select>
+            </div>
+            <div className="content-side">
+              <span>모집 종료 일자</span>
+              <input
+                type="date"
+                value={daedline}
+                onChange={onChange}
+                name="daedline"
+              />
+            </div>
+            <label>
+              <img className="profile-img" src={fileUrl} alt="" />
+              <input type="file" onChange={processImage} />
+            </label>
+          </S.ContentSide>
+        </S.Content>
+        <S.PostButton style={{ backgroundColor: post ? "blue" : "pink" }}>
+          게시하기
+        </S.PostButton>
+      </S.ContentWrapper>
+    </S.PostWrapper>
   );
 };
 
--- alavez/src/components/postMake/style.ts
+++ alavez/src/components/postMake/style.ts
@@ -1,3 +1,124 @@
 import styled from "styled-components";
 
-export const PostHeader = styled.div``;
+/* interface contentProps {
+  width: boolean;
+} */
+
+export const PostWrapper = styled.form`
+  margin: 100px auto;
+  width: 50%;
+  min-width: 600px;
+`;
+
+export const PostHeader = styled.div`
+  width: 100%;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid #80808054;
+
+  & input {
+    margin-right: 10px;
+    width: 25%;
+    height: 30px;
+    border: 2px solid #6f2dff;
+    padding: 3px;
+    outline: none;
+  }
+`;
+
+export const Category = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  min-width: 580px;
+`;
+
+export const ContentWrapper = styled.div`
+  width: 100%auto;
+  padding: 2% 0;
+  height: 600px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ContentTitle = styled.input`
+  padding: 0 2%;
+  width: 96%;
+  height: 40px;
+  border-radius: 3px;
+  background-color: #8080801a;
+  border: 1px solid #8080801a;
+  outline: none;
+  transition: all 0.3s;
+`;
+
+export const Content = styled.div`
+  margin-top: 30px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const ContentSub = styled.textarea`
+  padding: 2%;
+  min-width: 55%;
+  max-width: 55%;
+  min-height: 280px;
+  max-height: 280px;
+  outline: none;
+  border: none;
+  border-radius: 3px;
+  background-color: #8080801a;
+  border: 1px solid #8080801a;
+`;
+
+export const ContentSide = styled.div`
+  padding: 1%;
+  width: 30%;
+  height: 300px;
+  border: 1px solid #80808042;
+  box-shadow: 0 2px 5px #80808030;
+  border-radius: 3px;
+  display: flex;
+  flex-direction: column;
+  font-size: 15px;
+
+  .content-side {
+    margin-bottom: 20px;
+    color: gray;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+
+    & select,
+    input {
+      outline: none;
+      padding: 0 5px;
+      border: 2px solid #6f2dff;
+    }
+  }
+
+  & label {
+    .profile-img {
+      width: 100%;
+      height: 180px;
+      object-fit: cover;
+    }
+  }
+`;
+
+export const PostButton = styled.button`
+  margin: 50px auto;
+  width: 300px;
+  height: 30px;
+  color: white;
+  background-color: #6f2dff;
+  outline: none;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: all 0.3s;
+`;
