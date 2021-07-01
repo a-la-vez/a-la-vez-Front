fc34a1d5c0238f3468c6edfcf19a6f003c560cd7
강은빈 <kub0803@naver.com>
2021-07-01 11:06:02 +0900

    [MODIFY] 스터드 찾기 스타일 수정

--- alavez/src/components/groupSearch/groupSearch/GroupSearch.tsx
+++ alavez/src/components/groupSearch/groupSearch/GroupSearch.tsx
@@ -2,6 +2,7 @@ import * as S from "./style";
 import { useState } from "react";
 import { glass, groupImg2 } from "../../../assets";
 import { button, listDummy } from "../../../interfaces/group";
+import { ApplyForm } from "../../index";
 import { useRef } from "react";
 
 const GroupSearch = () => {
@@ -26,64 +27,66 @@ const GroupSearch = () => {
   };
 
   return (
-    <S.MainWrapper>
-      <S.Search>
-        <S.InputWrapper style={{ width: input ? "43%" : "" }}>
-          <input
-            ref={test}
-            type="text"
-            placeholder="검색을 해주세요"
-            onClick={inputClick}
-          />
-          <div className="glass-item">
-            <img src={glass} alt="검색 아이콘" />
-          </div>
-        </S.InputWrapper>
-        <S.Post>
-          <button>그룹 만들기</button>
-        </S.Post>
-      </S.Search>
-      <div
-        className="input-infomation"
-        style={{
-          width: input ? "30%" : " 0",
-        }}
-      >
-        <p>다양한 그룹을 검색하실 수 있습니다.</p>
-      </div>
-      <S.Category>
-        {button.map((button, index) => (
-          <button
-            className="default-button"
-            key={index}
-            style={{
-              backgroundColor:
-                button.id === selected
-                  ? "#6f2dff"
-                  : "rgba(128, 128, 128, 0.16)",
-              color: button.id === selected ? "white" : "black",
-            }}
-            onClick={() => categoryClickHandler(button)}
-          >
-            {button.name}
-          </button>
-        ))}
-      </S.Category>
-      <S.GroupWrapper>
-        {listDummy.map((list, index) => (
-          <S.GroupItem key={index}>
-            <div className="group-img">
-              <img src={groupImg2} alt="그룹 베너 사진"></img>
+    <>
+      <S.MainWrapper>
+        <S.Search>
+          <S.InputWrapper style={{ width: input ? "43%" : "" }}>
+            <input
+              ref={test}
+              type="text"
+              placeholder="검색을 해주세요"
+              onClick={inputClick}
+            />
+            <div className="glass-item">
+              <img src={glass} alt="검색 아이콘" />
             </div>
-            <S.GroupDescribe>
-              <a href="/group-detail/1">{list.Title}</a>
-              <span>{list.describe}</span>
-              <span>{list.Period}</span>
-            </S.GroupDescribe>
-          </S.GroupItem>
-        ))}
-      </S.GroupWrapper>
-    </S.MainWrapper>
+          </S.InputWrapper>
+          <S.Post>
+            <button>그룹 만들기</button>
+          </S.Post>
+        </S.Search>
+        <div
+          className="input-infomation"
+          style={{
+            width: input ? "30%" : " 0",
+          }}
+        >
+          <p>다양한 그룹을 검색하실 수 있습니다.</p>
+        </div>
+        <S.Category>
+          {button.map((button, index) => (
+            <button
+              className="default-button"
+              key={index}
+              style={{
+                backgroundColor:
+                  button.id === selected
+                    ? "#6f2dff"
+                    : "rgba(128, 128, 128, 0.16)",
+                color: button.id === selected ? "white" : "black",
+              }}
+              onClick={() => categoryClickHandler(button)}
+            >
+              {button.name}
+            </button>
+          ))}
+        </S.Category>
+        <S.GroupWrapper>
+          {listDummy.map((list, index) => (
+            <S.GroupItem key={index}>
+              <div className="group-img">
+                <img src={groupImg2} alt="그룹 베너 사진"></img>
+              </div>
+              <S.GroupDescribe>
+                <a href="/group-detail/1">{list.Title}</a>
+                <span>{list.describe}</span>
+                <span>{list.Period}</span>
+              </S.GroupDescribe>
+            </S.GroupItem>
+          ))}
+        </S.GroupWrapper>
+      </S.MainWrapper>
+    </>
   );
 };
 
