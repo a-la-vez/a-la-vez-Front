9b01b2094a1cfc5d70f4814bc6e70bf3007edba5
강은빈 <kub0803@naver.com>
2021-07-02 11:17:16 +0900

    [ADD] 신청목록 인터페이스 추가

---
+++ alavez/src/interfaces/applyList.ts
@@ -0,0 +1,92 @@
+export interface ApplyListType {
+  num: number;
+  groupName: string;
+  name: string;
+  date: number;
+  reason: string;
+  pass: boolean;
+}
+
+export const applyListDummy: ApplyListType[] = [
+  {
+    num: 1,
+    groupName: "스터디인데용",
+    name: "강은빈",
+    date: 20040209,
+    reason: "열심히 하겠습니다!!",
+    pass: true,
+  },
+  {
+    num: 1,
+    groupName: "스터디인데용",
+    name: "강은빈",
+    date: 20040209,
+    reason: "열심히 하겠습니다!!",
+    pass: true,
+  },
+  {
+    num: 1,
+    groupName: "스터디인데용",
+    name: "강은빈",
+    date: 20040209,
+    reason: "열심히 하겠습니다!!",
+    pass: true,
+  },
+  {
+    num: 1,
+    groupName: "스터디인데용",
+    name: "강은빈",
+    date: 20040209,
+    reason: "열심히 하겠습니다!!",
+    pass: true,
+  },
+  {
+    num: 1,
+    groupName: "스터디인데용",
+    name: "강은빈",
+    date: 20040209,
+    reason: "열심히 하겠습니다!!",
+    pass: true,
+  },
+
+  {
+    num: 1,
+    groupName: "스터디인데용",
+    name: "강은빈",
+    date: 20040209,
+    reason: "열심히 하겠습니다!!",
+    pass: true,
+  },
+  {
+    num: 1,
+    groupName: "스터디인데용",
+    name: "강은빈",
+    date: 20040209,
+    reason: "열심히 하겠습니다!!",
+    pass: true,
+  },
+  {
+    num: 1,
+    groupName: "스터디인데용",
+    name: "강은빈",
+    date: 20040209,
+    reason: "열심히 하겠습니다!!",
+    pass: true,
+  },
+  {
+    num: 1,
+    groupName: "스터디인데용",
+    name: "강은빈",
+    date: 20040209,
+    reason: "열심히 하겠습니다!!",
+    pass: true,
+  },
+  {
+    num: 1,
+    groupName: "스터디인데용",
+    name: "강은빈",
+    date: 20040209,
+    reason: "열심히 하겠습니다!!",
+    pass: true,
+  },
+];
