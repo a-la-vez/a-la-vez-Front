17e50da724f8b4dcaba7d8f88bf0080df890c0a8
강은빈 <kub0803@naver.com>
2021-07-02 11:13:35 +0900

    [ADD] 이미지 추가

--- alavez/src/assets/index.ts
+++ alavez/src/assets/index.ts
@@ -7,3 +7,5 @@ export { default as company } from "./company.svg";
 export { default as glass } from "./glass.svg";
 export { default as groupImg } from "./groupImg.jpg";
 export { default as groupImg2 } from "./groupImg2.jpg";
+export { default as close } from "./close.svg";
+export { default as logo } from "./logo.svg";
