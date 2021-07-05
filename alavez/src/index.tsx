35ab7326d1ce453c80b02a0c838592092ae932b2
강은빈 <kub0803@naver.com>
2021-07-05 23:19:17 +0900

    [MODIFY] index 수정

--- alavez/src/index.tsx
+++ alavez/src/index.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import reduxThunk from "redux-thunk";
+//import promiseMiddleware from "redux-promise";
+import rootReducer from "./store/reducers";
+
+const store = createStore(
+  rootReducer
+  // applyMiddleware(reduxThunk, promiseMiddleware)
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
