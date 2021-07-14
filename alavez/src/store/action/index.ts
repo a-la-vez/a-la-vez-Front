dbef3765b895345f97071017fcbc445d2c598539
강은빈 <kub0803@naver.com>
2021-07-14 23:47:53 +0900

    [ADD] 리듀서 추가

--- alavez/src/store/action/index.ts
+++ alavez/src/store/action/index.ts
@@ -1,11 +1,20 @@
-export const ADD_POSTING = "ADD_POSTING";
-export const EDIT_POSTING = "EDIT_POSTING";
-export const DELETE_POSTING = "DELETE_POSTING";
+export const ADD_POSTING = "ADD_POSTING"; // 게시글 등록
+export const EDIT_POSTING = "EDIT_POSTING"; // 게시글 수정
+export const DELETE_POSTING = "DELETE_POSTING"; // 게시글 삭제
+export const ADMISSION_APPLY = "ADMISSION_APPLY"; // 신청 승인
+export const REJECT_APPLY = "REJECT_APPLY"; // 신청 거절
 
 //승인 거부 리스트 삭제
 export const deletePosting = (id: number) => {
   return {
     type: DELETE_POSTING,
     id,
+  };
+};
+
+export const rejectApply = (id: number) => {
+  return {
+    type: "REJECT_APPLY",
+    payload: id,
   };
 };
---
+++ alavez/src/store/reducers/apply.ts
@@ -0,0 +1,12 @@
+const initialState = [
+  {
+    id: 1,
+  },
+];
+
+export const apply = (state = initialState, action: any) => {
+    switch(action.type) {
+        case "REJECT_APPLY" :
+            return 
+    }
+};
