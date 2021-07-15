5a86e564379e0a902301b875b655caa309b0d01f
강은빈 <kub0803@naver.com>
2021-07-15 23:35:34 +0900

    [ADD] 댓글 삭제 기능 추가

--- alavez/src/components/groupSearch/groupDetail/Comment.tsx
+++ alavez/src/components/groupSearch/groupDetail/Comment.tsx
@@ -1,14 +1,28 @@
 import React from "react";
-import { commentDummy, CommentType } from "../../../interfaces/group";
+import { useDispatch, useSelector } from "react-redux";
+import { Delete } from "../../../assets";
+import { CommentType } from "../../../interfaces/group";
+import { deleteComment } from "../../../store/action";
+import { RootState } from "../../../store/reducers";
 import * as S from "./style/style";
 
 const Comment = (comments: any, index: number) => {
+  const dispatch = useDispatch();
+
   return (
     <S.Comment key={index}>
       <div className="user-info">
         <img src={comments.comments.ImagePath} alt="사용자 프로필 사진" />
         <span>{comments.comments.UserName}</span>
         <span>{comments.comments.CreatedAt}</span>
+        <img
+          src={Delete}
+          alt="댓글삭제아이콘"
+          onClick={() => {
+            dispatch(deleteComment(comments.comments.Id));
+            alert("댓글이 삭제되었습니다.");
+          }}
+        ></img>
       </div>
       <div className="content">{comments.comments.Content}</div>
     </S.Comment>
@@ -16,17 +30,19 @@ const Comment = (comments: any, index: number) => {
 };
 
 const BottomComment = () => {
+  const comments = useSelector((state: RootState) => state.comments);
+
   return (
     <S.BottomContent>
       <S.CommentWrapper>
         <S.CommentHeader>
           <span>댓글</span>
-          <span>{commentDummy.length}개</span>
+          <span>{comments.length}개</span>
         </S.CommentHeader>
         <S.CommentInput>
           <input type="text" placeholder="댓글을 작성해주세요" />
         </S.CommentInput>
-        {commentDummy.map((comment: CommentType, index: number): any => (
+        {comments.map((comment: CommentType, index: number): any => (
           <Comment key={index} comments={comment} />
         ))}
         <button className="comment-more">더보기</button>
