f7e9dd91822137c6cb85560f8f44696212565fb5
강은빈 <kub0803@naver.com>
2021-07-25 23:44:13 +0900

    [ADD] react-query 추가

--- alavez/src/App.tsx
+++ alavez/src/App.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Router from "./router/Router";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <Router></Router>
+      <QueryClientProvider client={queryClient}>
+        <Router />
+      </QueryClientProvider>
     </>
   );
 }
