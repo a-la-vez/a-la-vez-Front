a326768c442149b61c591400533a0fce0c297945
강은빈 <kub0803@naver.com>
2021-08-27 23:59:49 +0900

    [ADD] group detail api add

--- alavez/src/components/groupSearch/groupDetail/GroupDetail.tsx
+++ alavez/src/components/groupSearch/groupDetail/GroupDetail.tsx
@@ -1,4 +1,7 @@
+import axios from "axios";
+import { useEffect } from "react";
 import { useState } from "react";
+import { useMutation, useQuery } from "react-query";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store/reducers";
 import {
@@ -9,19 +12,30 @@ import {
 } from "../../index";
 import * as S from "./style/style";
 
-const GroupDetail = () => {
+const GroupDetail = ({ match }: any) => {
   const [apply, setApply] = useState<boolean>(false);
   const groupDetail = useSelector((state: RootState) => state.posts);
+
+  const { isLoading, error, data } = useQuery("postDetail", () =>
+    axios(`https://qovh.herokuapp.com/post/${match.params.id}`)
+  );
+
+  console.log(data?.data.post);
 
   return (
     <>
       <ApplyForm apply={apply} setApply={setApply} />
       <S.MainWrapper>
-        <S.GroupDetailContent>
-          <GroupDetailHeader groupDetail={groupDetail} />
-          <Content setApply={setApply} groupDetail={groupDetail} />
-          <BottomComment />
-        </S.GroupDetailContent>
+        {error && <>데이터를 불러올 수 없습니다.</>}
+        {isLoading ? (
+          <>잠시만 기달려주세요.</>
+        ) : (
+          <S.GroupDetailContent>
+            <GroupDetailHeader groupDetail={groupDetail} />
+            <Content setApply={setApply} groupDetail={groupDetail} />
+            <BottomComment />
+          </S.GroupDetailContent>
+        )}
       </S.MainWrapper>
     </>
   );
