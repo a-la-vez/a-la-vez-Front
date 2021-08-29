d5bbc6677aaaea8f2a496498d118d62d0f6fabae
강은빈 <kub0803@naver.com>
2021-08-29 23:45:16 +0900

    [MODIFY] group detail header api contents

--- alavez/src/components/groupSearch/groupDetail/GroupDetailHeader.tsx
+++ alavez/src/components/groupSearch/groupDetail/GroupDetailHeader.tsx
@@ -2,19 +2,20 @@ import * as S from "./style/style";
 import { Delete } from "../../../assets";
 import { groupDetailProps } from "../../../interfaces/group";
 
-const GroupDetailHeader = ({ groupDetail }: groupDetailProps) => {
+const GroupDetailHeader = ({ groupDetail, postDelete }: groupDetailProps) => {
+  console.log(groupDetail);
   return (
     <S.GroupDetailHeader>
       <div className="category">
-        <div className="category-item">{groupDetail.Category}</div>
+        <div className="category-item">{groupDetail?.category}</div>
       </div>
       <div className="date">
         <a href="/post" className="modify-group">
           스터디 수정
         </a>
-        <span>작성일 : {groupDetail.CreatedAt}</span>
+        <span>작성일 : {groupDetail.createdAt}</span>
         <span>수정일 : {groupDetail.UpdatedAt}</span>
-        <img src={Delete} alt="삭제아이콘" />
+        <img src={Delete} onClick={() => postDelete()} alt="삭제아이콘" />
       </div>
     </S.GroupDetailHeader>
   );
