44924a5ad5f36c561b5eb28f57afc6bbfdbc7cc5
강은빈 <kub0803@naver.com>
2021-09-02 23:34:24 +0900

    [DELETE] 리덕스 삭제

--- alavez/src/store/action/actionType.ts
+++
@@ -1,7 +0,0 @@
-import { addComment, deleteComment, deletePosting, rejectApply } from ".";
-
-export type ApplyAction = ReturnType<typeof rejectApply>;
-export type postAction = ReturnType<typeof deletePosting>;
-export type CommentAction =
-  | ReturnType<typeof deleteComment>
-  | ReturnType<typeof addComment>;
